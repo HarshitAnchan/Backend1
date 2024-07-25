e243059e3015a7a1adbd384985fa0de35e9716fd
harshitanchan <harshitanchan647@gmail.com>
2024-07-25 20:13:08 +0530

    CRUD operation MongoDB in detail

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
 const express= require('express');
 const app = express();
-const userModel = require('./usermodel');
-const usermodel = require('./usermodel');
+const path = require('path');
+
+const userModel = require('./models/user');
+
+app.set("view engine", "ejs")
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get("/", (req, res)=>{
- res.send("successfull")
+ res.render("index")
 })
 
-app.get("/create", async (req, res)=>{
-  let createduser =  await userModel.create({
-    name: "harshit",
-    email: "harshit@gmail.com",
-    username: "harshit"
-   })
-      
-
-    res.send(createduser)
-
-    })
-
     
-app.get("/read",async (req, res)=>{
-    let users = await userModel.find();
-    res.send(users)
+app.get("/read", async(req, res)=>{
+  let users =  await userModel.find()
+    res.render("read", {users})
+})    
+
+app.get("/edit/:userid", async (req, res)=>{
+    let user = await userModel.findOne({_id: req.params.userid})
+        res.render("edit",{user})
+    
+  })
+
+
+  app.post("/update/:userid", async (req, res)=>{
+    let{image, name, email} = req.body
+    let user = await userModel.findOneAndUpdate({_id: req.params.userid}, {image, name, email}, {new: true})
+        res.redirect("/read")
+    
+  })
+
+
+app.get("/delete/:id",async (req, res)=>{
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect( "/read")
+})    
+
+app.post("/create", async(req, res)=>{
+    let {name, email, image} = req.body;
+   
+   let createdUser = await userModel.create({
+        name,
+        email,
+        image
+
+    });
+
+    res.redirect("/read")
+
 })    
 
 
 
-app.get("/update", async (req, res)=>{
-      let updateduser = await userModel.findOneAndUpdate({username: "harshit"}, {name: "Alisha"}, {new: true})
-          res.send(updateduser)
-      
-    })
 
 
-app.get("/delete",async (req, res)=>{
-    let users = await userModel.findOneAndDelete({name: "harshit"});
-    res.send(users)
-})    
+
+
 
 
 
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/tendercoconut`);
+
+const userSchema = mongoose.Schema({
+    image: String,
+    name: String,
+    email: String
+})
+
+module.exports = mongoose.model("user", userSchema);
+
+
+
+
+
+
+
+
